499d4bfdd608b8cdb398c3afc003f87a9cbdb925
Rafat Bin Islam <63665144+Rafat-Bin@users.noreply.github.com>
2026-02-10 14:30:42 -0700

    Fix syntax error in Customer class property

--- Module 2-Codes/Lesson-16_EF_Core_intro/class code example/EfCoreIntro/EfCoreIntro/Customer.cs
+++ Module 2-Codes/Lesson-16_EF_Core_intro/class code example/EfCoreIntro/EfCoreIntro/Customer.cs
@@ -6,6 +6,6 @@ namespace EfCoreIntro
     public class Customer
     {
         public int Id { get; set; }     // Primary key
-        public string Name { get; set; };
+        public string Name { get; set; }
     }
 }
