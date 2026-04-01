bbdcc508ff956058ceb908a0fb2d5569d0bfb7ce
Rafat Bin Islam <63665144+Rafat-Bin@users.noreply.github.com>
2026-03-31 20:40:00 -0600

    added new files

---
+++ Module 4-codes/Lesson35/lesson35/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace lesson35
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "lesson35" };
+        }
+    }
+}
---
+++ Module 4-codes/Lesson35/lesson35/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace lesson35
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
