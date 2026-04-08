df694cf37b4ca3767747ee5efe3ae44ad6513885
Rafat Bin Islam <63665144+Rafat-Bin@users.noreply.github.com>
2026-04-08 17:41:57 -0600

    l38 added

---
+++ Module 4-codes/Lesson38/Lesson38/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson38
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Module 4-codes/Lesson38/Lesson38/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Lesson38
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
